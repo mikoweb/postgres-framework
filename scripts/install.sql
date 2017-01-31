52337e19fe66d94f7cbbd1b75700b475dfe69c39
Rafał Mikołajun <rafal@mikoweb.pl>
2017-01-31 17:23:45 +0100

    script update

--- scripts/install.sql
+++ scripts/install.sql
@@ -983,7 +983,6 @@ END $$;
 CREATE EXTENSION IF NOT EXISTS "plv8";
 CREATE SCHEMA IF NOT EXISTS framework;
 
--- RFC 3987
 -- http://stackoverflow.com/questions/2568842/jquery-url-validator
 -- http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without
 CREATE OR REPLACE FUNCTION framework.check_url(email text) RETURNS bool AS $$
