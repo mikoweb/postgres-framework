866105ba9a4763df4fcb8e9057e90ea950e25e07
Rafał Mikołajun <rafal@mikoweb.pl>
2017-01-31 17:22:38 +0100

    update comment

--- src/functions/check_url.sql
+++ src/functions/check_url.sql
@@ -1,4 +1,3 @@
--- RFC 3987
 -- http://stackoverflow.com/questions/2568842/jquery-url-validator
 -- http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without
 CREATE OR REPLACE FUNCTION framework.check_url(email text) RETURNS bool AS $$
