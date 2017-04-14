43116d6e39067b4e1cbea2cb14de16ae1db6f558
Rafał Mikołajun <rafal@mikoweb.pl>
2017-04-15 00:00:41 +0200

    remove plv8 extension

--- scripts/install.sql
+++ scripts/install.sql
@@ -971,23 +971,17 @@ $$
 LANGUAGE plpgsql;
 
 
-DO $$
-DECLARE plv8_exists integer;
-BEGIN
-    SELECT COUNT(name) INTO plv8_exists FROM pg_available_extensions() WHERE name = 'plv8';
-    IF plv8_exists = 0 THEN
-        RAISE EXCEPTION 'Extension plv8 is not installed. Please run: sudo apt-get install postgresql-{your-postgresql-version-here}-plv8';
-    END IF;
-END $$;
-
-CREATE EXTENSION IF NOT EXISTS "plv8";
 CREATE SCHEMA IF NOT EXISTS framework;
 
 -- http://stackoverflow.com/questions/2568842/jquery-url-validator
 -- http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without
-CREATE OR REPLACE FUNCTION framework.check_url(email text) RETURNS bool AS $$
-    return /((([A-Za-z]+:(?:\/\/)+)(?:[\-;:&=\+\$,\w]+@)?)(.)+)/i.test(email);
-$$ LANGUAGE plv8 IMMUTABLE STRICT;
+CREATE OR REPLACE FUNCTION
+    framework.check_url(email text)
+    RETURNS bool AS $$
+BEGIN
+    RETURN email ~* '((([A-Za-z]+:(?:\/\/)+)(?:[\-;:&=\+\$,\w]+@)?)(.)+)';
+END
+$$ LANGUAGE plpgsql;
 
 -- Create unique string based first argument.
 CREATE OR REPLACE FUNCTION
--- src/functions/check_url.sql
+++ src/functions/check_url.sql
@@ -1,5 +1,9 @@
 -- http://stackoverflow.com/questions/2568842/jquery-url-validator
 -- http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without
-CREATE OR REPLACE FUNCTION framework.check_url(email text) RETURNS bool AS $$
-    return /((([A-Za-z]+:(?:\/\/)+)(?:[\-;:&=\+\$,\w]+@)?)(.)+)/i.test(email);
-$$ LANGUAGE plv8 IMMUTABLE STRICT;
+CREATE OR REPLACE FUNCTION
+    framework.check_url(email text)
+    RETURNS bool AS $$
+BEGIN
+    RETURN email ~* '((([A-Za-z]+:(?:\/\/)+)(?:[\-;:&=\+\$,\w]+@)?)(.)+)';
+END
+$$ LANGUAGE plpgsql;
