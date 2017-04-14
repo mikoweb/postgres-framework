5a59f724b2a4abf627c02b79a7f294a6e7d666bf
Rafał Mikołajun <rafal@mikoweb.pl>
2017-04-15 00:15:45 +0200

    new function framework.check_http_url

--- scripts/install.sql
+++ scripts/install.sql
@@ -976,6 +976,16 @@ CREATE SCHEMA IF NOT EXISTS framework;
 -- http://stackoverflow.com/questions/2568842/jquery-url-validator
 -- http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without
 CREATE OR REPLACE FUNCTION
+    framework.check_http_url(email text)
+    RETURNS bool AS $$
+BEGIN
+    RETURN email ~* '((((http|https)+:(?:\/\/)+)(?:[\-;:&=\+\$,\w]+@)?)(.)+)';
+END
+$$ LANGUAGE plpgsql;
+
+-- http://stackoverflow.com/questions/2568842/jquery-url-validator
+-- http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without
+CREATE OR REPLACE FUNCTION
     framework.check_url(email text)
     RETURNS bool AS $$
 BEGIN
--- scripts/tests_install.sql
+++ scripts/tests_install.sql
@@ -1,3 +1,79 @@
+CREATE OR REPLACE FUNCTION unit_tests.check_http_url_test()
+RETURNS test_result AS $$
+DECLARE message test_result;
+DECLARE result boolean;
+BEGIN
+    SELECT check_http_url INTO result FROM framework.check_http_url('localhost');
+    IF result = TRUE THEN
+        SELECT assert.fail('Value "localhost" is invalid url.') INTO message;
+        RETURN message;
+    END IF;
+
+    SELECT check_http_url INTO result FROM framework.check_http_url('http://');
+    IF result = TRUE THEN
+        SELECT assert.fail('Value "http://" is invalid url.') INTO message;
+        RETURN message;
+    END IF;
+
+    SELECT check_http_url INTO result FROM framework.check_http_url('google.pl');
+    IF result = TRUE THEN
+        SELECT assert.fail('Value "google.pl" is invalid url.') INTO message;
+        RETURN message;
+    END IF;
+
+    SELECT check_http_url INTO result FROM framework.check_http_url('www.google.pl');
+    IF result = TRUE THEN
+        SELECT assert.fail('Value "www.google.pl" is invalid url.') INTO message;
+        RETURN message;
+    END IF;
+
+    SELECT check_http_url INTO result FROM framework.check_http_url('//google.pl');
+    IF result = TRUE THEN
+        SELECT assert.fail('Value "//google.pl" is invalid url.') INTO message;
+        RETURN message;
+    END IF;
+
+    SELECT check_http_url INTO result FROM framework.check_http_url('http//google.pl');
+    IF result = TRUE THEN
+        SELECT assert.fail('Value "http//google.pl" is invalid url.') INTO message;
+        RETURN message;
+    END IF;
+
+    SELECT check_http_url INTO result FROM framework.check_http_url('https://google.pl');
+    IF result = FALSE THEN
+        SELECT assert.fail('Value "https://google.pl" is valid url.') INTO message;
+        RETURN message;
+    END IF;
+
+    SELECT check_http_url INTO result FROM framework.check_http_url('http://www.google.pl');
+    IF result = FALSE THEN
+        SELECT assert.fail('Value "http://www.google.pl" is valid url.') INTO message;
+        RETURN message;
+    END IF;
+
+    SELECT check_http_url INTO result FROM framework.check_http_url('abcdefg://www.google.pl');
+    IF result = TRUE THEN
+        SELECT assert.fail('Value "abcdefg://www.google.pl" is invalid url.') INTO message;
+        RETURN message;
+    END IF;
+
+    SELECT check_http_url INTO result FROM framework.check_http_url('abcdefg://google.pl');
+    IF result = TRUE THEN
+        SELECT assert.fail('Value "abcdefg://google.pl" is invalid url.') INTO message;
+        RETURN message;
+    END IF;
+
+    SELECT check_http_url INTO result FROM framework.check_http_url('http://localhost/test/test?gfdgfdg#ok');
+    IF result = FALSE THEN
+        SELECT assert.fail('Value "http://localhost/test/test?gfdgfdg#ok" is valid url.') INTO message;
+        RETURN message;
+    END IF;
+
+    SELECT assert.ok('framework.check_http_url is OK') INTO message;
+    RETURN message;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE FUNCTION unit_tests.check_url_test()
 RETURNS test_result AS $$
 DECLARE message test_result;
