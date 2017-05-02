b60fc897f07738de1a7a7ebdc96268678e279779
Rafał Mikołajun <rafal@mikoweb.pl>
2017-05-02 18:51:24 +0200

    add function array_from_json

--- scripts/install.sql
+++ scripts/install.sql
@@ -983,6 +983,29 @@ CREATE OR REPLACE FUNCTION framework.array_distinct(
     WHERE CASE WHEN $2 THEN x IS NOT NULL ELSE true END;
 $f$ LANGUAGE SQL IMMUTABLE;
 
+-- Convert json array to TEXT[]
+CREATE OR REPLACE FUNCTION
+    framework.array_from_json(in json_arr json)
+RETURNS text[] AS $$
+    DECLARE arr text[];
+    DECLARE item json;
+    DECLARE item_value text;
+BEGIN
+    FOR item IN SELECT * FROM json_array_elements(json_arr)
+    LOOP
+        item_value := item::text;
+        IF substring(item_value, 1, 1) <> '"' THEN
+            RAISE EXCEPTION 'Element is not string'
+            USING HINT = 'Acceptable only array of string. Found element value: ' || item_value;
+        END IF;
+
+        arr := array_append(arr, substring(item_value, 2, char_length(item_value) - 2));
+    END LOOP;
+
+    RETURN arr;
+END
+$$ LANGUAGE plpgsql;
+
 -- http://stackoverflow.com/questions/2568842/jquery-url-validator
 -- http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without
 CREATE OR REPLACE FUNCTION
