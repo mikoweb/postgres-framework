73bae621db6f11286074eeeb3bb937a9a94e260e
Rafał Mikołajun <rafal@mikoweb.pl>
2017-06-08 21:50:24 +0200

    remove old javascript version

--- scripts/install.js
+++
@@ -1,6 +0,0 @@
-const psqlCmd = require('./helpers/psql-cmd'),
-    program = psqlCmd.createProgram('0.0.1');
-
-const child = psqlCmd.startImport(program, __dirname + '/install.sql');
-
-psqlCmd.execStandardCase(child, 'Installation successful.');
--- scripts/uninstall.js
+++
@@ -1,6 +0,0 @@
-const psqlCmd = require('./helpers/psql-cmd'),
-    program = psqlCmd.createProgram('0.0.1');
-
-const child = psqlCmd.startImport(program, __dirname + '/uninstall.sql');
-
-psqlCmd.execStandardCase(child, 'Uninstall successful.');
