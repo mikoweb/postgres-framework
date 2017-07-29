fabf84a8d0306cb12ef743637b096249130df5cf
Rafał Mikołajun <rafal@mikoweb.pl>
2017-07-29 16:05:45 +0200

    rename variable

--- src/functions/check_http_url.sql
+++ src/functions/check_http_url.sql
@@ -1,9 +1,9 @@
 -- http://stackoverflow.com/questions/2568842/jquery-url-validator
 -- http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without
 CREATE OR REPLACE FUNCTION
-    framework.check_http_url(email text)
+    framework.check_http_url(url text)
     RETURNS bool AS $$
 BEGIN
-    RETURN email ~* '((((http|https)+:(?:\/\/)+)(?:[\-;:&=\+\$,\w]+@)?)(.)+)';
+    RETURN url ~* '((((http|https)+:(?:\/\/)+)(?:[\-;:&=\+\$,\w]+@)?)(.)+)';
 END
 $$ LANGUAGE plpgsql;
--- src/functions/check_url.sql
+++ src/functions/check_url.sql
@@ -1,9 +1,9 @@
 -- http://stackoverflow.com/questions/2568842/jquery-url-validator
 -- http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without
 CREATE OR REPLACE FUNCTION
-    framework.check_url(email text)
+    framework.check_url(url text)
     RETURNS bool AS $$
 BEGIN
-    RETURN email ~* '((([A-Za-z]+:(?:\/\/)+)(?:[\-;:&=\+\$,\w]+@)?)(.)+)';
+    RETURN url ~* '((([A-Za-z]+:(?:\/\/)+)(?:[\-;:&=\+\$,\w]+@)?)(.)+)';
 END
 $$ LANGUAGE plpgsql;
